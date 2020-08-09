baf141b6c5c312200d79cf5f7054e1aacc065d7e
Sohail Shaikh <sohail28195@gmail.com>
2020-08-09 21:00:03 +0530

    added title limiter :zap:

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import Search from "./models/Search";
-
+import * as searchView from "./views/searchView";
+import { elements } from "./views/base";
 /*
 - Global state of the app
 - Search object
@@ -11,23 +12,25 @@ const state = {};
 
 const controlSearch = async () => {
   //Get query from view
-  const query = "pizza"; // TODO: Accept from form
+  const query = searchView.getInput(); // TODO: Accept from form
 
   if (query) {
     //New  search object and add to state
     state.search = new Search(query);
 
     //Prepare UI for results
+    searchView.clearInput();
+    searchView.clearResult();
 
     //Search for recipes
     await state.search.getResults();
 
     //Render results on UI
-    console.log(state.search.result);
+    searchView.renderResults(state.search.result);
   }
 };
 
-document.querySelector(".search").addEventListener("submit", (e) => {
+elements.searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
   controlSearch();
 });
