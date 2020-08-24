905242a69e17fe98210888e42cc83abeec9b7867
Sohail Shaikh <sohail28195@gmail.com>
2020-08-24 19:29:49 +0530

    servings incremented/decremented :zap:

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -69,12 +69,12 @@ export const renderRecipe = (recipe) => {
             <span class="recipe__info-text"> servings</span>
 
             <div class="recipe__info-buttons">
-                <button class="btn-tiny">
+                <button class="btn-tiny btn-decrease">
                     <svg>
                         <use href="img/icons.svg#icon-circle-with-minus"></use>
                     </svg>
                 </button>
-                <button class="btn-tiny">
+                <button class="btn-tiny btn-increase">
                     <svg>
                         <use href="img/icons.svg#icon-circle-with-plus"></use>
                     </svg>
@@ -121,4 +121,17 @@ export const renderRecipe = (recipe) => {
         </a>
     </div>`;
   elements.recipe.insertAdjacentHTML("afterbegin", markup);
+};
+
+export const updateServingsIngredients = (recipe) => {
+  //update servings
+  document.querySelector(".recipe__info-data--people").textContent =
+    recipe.servings;
+
+  //update ingredients
+  const countElements = Array.from(document.querySelectorAll(".recipe__count"));
+
+  countElements.forEach((el, i) => {
+    el.textContent = formatCount(recipe.ingredients[i].count);
+  });
 };
